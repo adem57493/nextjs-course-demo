d7ef5d97b79f9763333dc38a188ccfcf41eb800b
adem57493 <150789425+adem57493@users.noreply.github.com>
2025-04-01 12:53:28 +0300

    Delete tic-tac-toe-starting-project-e directory

--- tic-tac-toe-starting-project-e/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import reactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-
-const entryPoint =document.getElementById('root');
-reactDOM.createRoot(entryPoint).render(<App/>);
